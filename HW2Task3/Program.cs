eeb7dcf50dba41bdf62eb7ef79d9659e2ee71d24
Valeriy Ryzhkov <VRyzhkov@SP.GL.RUS>
2022-07-01 11:32:51 +0300

    add hw2t3

---
+++ HW2Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и
+// удаляет вторую цифру этого числа.
+
+  
+int A = new Random().Next(99, 1000); 
+Console.WriteLine(A);
+
+int B = A%10;
+int C = (A/10)%10;
+int D = A/100;
+     
+Console.Write(D);
+Console.Write(B);
+
