b09749df124b4c766cd99d636934a10126694f21
valery_ryzhkov <valery.ryzhkov@gmail.com>
2022-07-02 11:21:42 +0300

    add hw2t1

---
+++ HW2Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+
+int Promt(string message)
+{
+    Console.Write (message);
+    int a = int.Parse(Console.ReadLine());
+    return (a);
+}
+
+int A = Promt ( "enter A >");
+int C = (A/10)%10;
+//int D = A/100;
+if (A < 99 & A >1000)
+{
+     Console.WriteLine("wrong");
+}
+
+else 
+{Console.WriteLine(C);}
+
