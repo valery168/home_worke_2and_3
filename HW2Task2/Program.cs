afc66a52af35917b64f1a624816a9ff9567ddcc3
valery_ryzhkov <valery.ryzhkov@gmail.com>
2022-07-02 11:19:54 +0300

    add hw2t2

---
+++ HW2Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+int Promt(string message)
+{
+    Console.Write (message);
+    int a = int.Parse(Console.ReadLine());
+    return (a);
+}
+
+int A = Promt ( "enter A >");
+if (1<A && A <6)
+{
+    Console.WriteLine(" it is working day");
+    }
+
+if (5<A && A <8)
+{
+    Console.WriteLine(" it is weekend");
+    }
+ else Console.WriteLine("enter number bigger than 0 and les than 8");
+
+
