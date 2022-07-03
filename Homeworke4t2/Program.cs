e3f6639c3dfcdad4f1bd2f42ebf3cdb71bec399e
valery_ryzhkov <valery.ryzhkov@gmail.com>
2022-07-03 13:06:25 +0300

    add hw4.t2

---
+++ Homeworke4t2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+int Promt(string message)
+{
+    Console.Write (message);
+    int a = int.Parse(Console.ReadLine());
+    return (a);
+}
+int CalcSumDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+    return sum;
+}
+int num = Promt("введите число > ");
+int summ = CalcSumDigits(num);
+Console.WriteLine ($" сумма чисел > {summ} ");
+
+
