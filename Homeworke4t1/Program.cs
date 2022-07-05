2283dc3da7022c8a0005717855509a2196c7ee23
Valeriy Ryzhkov <VRyzhkov@SP.GL.RUS>
2022-07-05 19:54:12 +0300

    add hw4t1

---
+++ Homeworke4t1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+int Promt(string message)
+{
+    Console.Write (message);
+    int a = int.Parse(Console.ReadLine());
+    return (a);
+}
+int A = Promt("введите число А > ");
+int B = Promt("введите число В > ");
+int count = 0;
+Console.WriteLine( Math.Pow(A, B));
